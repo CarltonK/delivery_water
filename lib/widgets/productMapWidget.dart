4e76b72fc360e65157a25cc47a6e75aa1a9bfe38
Mark <mcarlton33@gmail.com>
2021-02-02 21:28:21 +0300

    replace deprecated property

--- lib/widgets/productMapWidget.dart
+++ lib/widgets/productMapWidget.dart
@@ -41,7 +41,7 @@ class _ProductMapWidgetState extends State<ProductMapWidget> {
         },
       ),
     );
-    Scaffold.of(context).showSnackBar(snackBar);
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   void _onMapCreated(GoogleMapController controller) {
