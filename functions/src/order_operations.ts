ead1ec1def691e1fa15d9156858fc6259f7841fa
Mark <mcarlton33@gmail.com>
2021-02-14 18:59:38 +0300

    listen for on create events

--- functions/src/order_operations.ts
+++ functions/src/order_operations.ts
@@ -9,15 +9,15 @@ export const newOrder = functions.runWith({
     timeoutSeconds: 30,
 }).region('europe-west3').firestore
     .document('/orders/{order}')
-    .onUpdate(async snapshot => {
+    .onCreate(async snapshot => {
         const batch = db.batch()
         try {
             const key = process.env.API_KEY
             // Get Location Name
-            const oID = snapshot.after.id
-            const total: number = snapshot.after.get('grandtotal')
-            const client = snapshot.after.get('client')
-            const coordinates = snapshot.after.get('location')
+            const oID = snapshot.id
+            const total: number = snapshot.get('grandtotal')
+            const client = snapshot.get('client')
+            const coordinates = snapshot.get('location')
 
             const latitude: number = coordinates['latitude']
             const longitude: number = coordinates['longitude']
@@ -51,7 +51,7 @@ export const newOrder = functions.runWith({
                         const orderRef = db.collection('orders').doc(oID)
                         const suppliers: string[] = []
                         const details: Map<string, string>[] = []
-                        const products: any[] = snapshot.after.get('products')
+                        const products: any[] = snapshot.get('products')
                         products.forEach(product => {
                             // Send key info to avoid querying for the product document 
                             const supplier: string = product['supplier']
