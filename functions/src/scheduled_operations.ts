f623722af609ed29305f1c508f16e6c9d53f1c6d
Mark <mcarlton33@gmail.com>
2021-02-14 19:18:14 +0300

    set correct cloud scheduler time

--- functions/src/scheduled_operations.ts
+++ functions/src/scheduled_operations.ts
@@ -1,10 +1,13 @@
 import * as functions from 'firebase-functions'
 // import { db } from './index'
 
+// Every day at 1800 HRS
+// ("0 18 * * *")
+
 export const dailySupplierPayment = functions.runWith({
     memory: '512MB',
     timeoutSeconds: 30,
-}).region('europe-west3').pubsub.schedule('* * * * *')
+}).region('europe-west3').pubsub.schedule('0 18 * * *')
     .timeZone('Africa/Nairobi')
     .onRun(async (context: functions.EventContext) => {
         console.log('This will be used to pay suppliers')
